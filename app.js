80590e36258de07e79408ffd54f8e1c7e2a67d36
Justin Ziegler <justinziegler345@gmail.com>
2019-01-30 07:48:31 -0500

    minor modifications to add gcloud deployment at https://p-project-230210.appspot.com

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const loki = require('lokijs');
 const uuid = require('uuid/v4');
 
-const PORT = 5575;
-const HOST = '192.168.1.12';
+const PORT = 8080;
+const HOST = '127.0.0.1';
 
 //App
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
 const db = new loki('./data/loki.json');
+db.autosave = true;
 const users = db.addCollection('users');
 const logs = db.addCollection('logs');
 
+app.get('/', (req, res) => {
+    res.send("Ziegler");
+});
+
 app.post('/login', (req, res) => {
     console.log('/login');
-    console.log(req.body);
 
     if (req.body.email === undefined || req.body.p_hash === undefined) {
         // bad request
@@ -22,20 +30,21 @@ app.post('/login', (req, res) => {
             error: 400,
             error_message: 'The request was bad, please make sure the email and HASH are included.'
         });
+        return;
     }
 
     const result = users.find({email: req.body.email});
-    if (result.p_hash !== req.body.p_hash) {
-        // bad request
+    console.log(result);
+    if (result.length !== 0 && result[0].p_hash == req.body.p_hash) {
+        res.send({
+            valid: true,
+            uuid: result[0].uuid
+        });
+    } else {
         res.send({
             valid: false,
             error: 469,
             error_message: 'Bad Password'
-        });
-    } else {
-        res.send({
-            valid: true,
-            uuid: result.uuid
         });
     }
 
@@ -43,8 +52,7 @@ app.post('/login', (req, res) => {
 
 app.post('/signup', (req, res) => {
     console.log('/login');
-    console.log(req.body);
-
+    
     if (req.body.email === undefined || req.body.p_hash === undefined) {
         // bad request
         res.send({
@@ -61,6 +69,7 @@ app.post('/signup', (req, res) => {
             error: 470,
             error_message: 'User already exists.'
         });
+        return;
     }
     const p_uuid = uuid();
     users.insert(
@@ -76,5 +85,25 @@ app.post('/signup', (req, res) => {
     });
 });
 
-app.listen(PORT, HOST);
-console.log('Running on http://%s:%d', HOST, PORT);
+app.put('/:userId/log', (req, res) => {
+    console.log(`${req.params.userId} logged a weather event.`);
+    console.log(req);
+    
+    logs.insertOne({
+        uuid: req.params.userId,
+        weather: req.query.weather,
+        temp: req.query.temp
+    });
+
+    res.send({
+        valid: true
+    })
+});
+
+app.get('/:userId/logs', (req, res) => {
+    const results = logs.find({uuid : req.params.userId});
+    res.send(results);
+});
+
+app.listen(PORT);
+console.log(`Running on ${PORT}`);
